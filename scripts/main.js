7976780531ff941d7b47d030df35f4e259ecd31d
Alexandra Vasco✩ <alexavascolopera@hotmail.com>
2025-05-12 23:37:25 -0500

    update login

--- scripts/main.js
+++ scripts/main.js
@@ -56,4 +56,24 @@ document.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('#formulario_login')) LogIn();
   
   updateSessionUI();
+
+  // Función para manejar la visibilidad de la contraseña
+  const togglePasswordButtons = document.querySelectorAll('.toggle-password');
+  
+  togglePasswordButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      const passwordInput = this.previousElementSibling;
+      const icon = this.querySelector('i');
+      
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+      } else {
+        passwordInput.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+      }
+    });
+  });
 });
